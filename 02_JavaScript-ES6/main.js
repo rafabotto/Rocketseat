6b5aa157b67ed4eda86ff50e80b46155d2e25b47
rafabotto <rbdf91@gmail.com>
2019-04-25 11:20:44 -0300

    operations with arrays using the arr.map function

--- 02_JavaScript-ES6/main.js
+++ 02_JavaScript-ES6/main.js
@@ -1,7 +1,31 @@
-class Matematica{
-    static soma(a, b){
-        return a + b;
-    }
-}
+const arr = [1, 3, 4, 5, 8, 9];
 
-console.log(Matematica.soma(1,2));
+const newArr = arr.map(function(item, index){
+    return item + index;
+});
+
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+
+console.log(sum);
+
+// const filter = arr.filter(function(item){
+//     if(item % 2 === 0){
+//         return item;
+//     }
+// });
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 4;
+});
+
+console.log(find);
