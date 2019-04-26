5ba87c6382a52fef8b30b1e5c4caf3d3cc0fa716
rafabotto <rbdf91@gmail.com>
2019-04-26 09:52:19 -0300

    restructuring of javascript object

--- 02_JavaScript-ES6/bundle.js
+++ 02_JavaScript-ES6/bundle.js
@@ -1,10 +1,27 @@
 "use strict";
 
-var soma = function soma() {
-  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
-  return a + b;
+var usuario = {
+  nome: 'Rafael',
+  idade: 27,
+  endereco: {
+    cidade: 'Campinas',
+    estado: 'SP'
+  }
 };
+console.log(usuario); // const nome = usuario.nome;
+// const idade = usuario.idade;
+// const cidade = usuario.endereco.cidade;
 
-console.log(soma(1));
-console.log(soma());
+var nome = usuario.nome,
+    idade = usuario.idade,
+    cidade = usuario.endereco.cidade;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostraNome(_ref) {
+  var nome = _ref.nome;
+  console.log(nome, idade);
+}
+
+mostraNome(usuario);
--- 02_JavaScript-ES6/main.js
+++ 02_JavaScript-ES6/main.js
@@ -1,4 +1,25 @@
-const soma = (a = 3, b = 6) => a + b;
+const usuario = {
+    nome: 'Rafael',
+    idade: 27,
+    endereco: {
+        cidade: 'Campinas',
+        estado: 'SP',
+    },
+};
 
-console.log(soma(1));
-console.log(soma());
+console.log(usuario);
+
+// const nome = usuario.nome;
+// const idade = usuario.idade;
+// const cidade = usuario.endereco.cidade;
+
+const { nome, idade, endereco: { cidade } } = usuario;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostraNome({nome}){
+    console.log(nome, idade);
+}
+
+mostraNome(usuario);
