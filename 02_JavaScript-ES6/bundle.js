ad0bef82548e546c27f31f23a94cdb2320a090a0
rafabotto <rbdf91@gmail.com>
2019-04-29 10:31:12 -0300

    exercise 04 from module 01

---
+++ 02_JavaScript-ES6/bundle.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome);
+console.log(cidade);
+console.log(estado);
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo({
+  nome: 'Diego',
+  idade: 23
+}));
