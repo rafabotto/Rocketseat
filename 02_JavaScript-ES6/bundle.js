c96dadc2e0c7ddd5d4dab6e29ae5c246c57e09b3
rafabotto <rbdf91@gmail.com>
2019-04-29 10:42:11 -0300

    exerciso 05 module 01

--- 02_JavaScript-ES6/bundle.js
+++ 02_JavaScript-ES6/bundle.js
@@ -1,27 +1,53 @@
 "use strict";
 
-var empresa = {
-  nome: 'Rocketseat',
-  endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC'
-  }
-};
-var nome = empresa.nome,
-    _empresa$endereco = empresa.endereco,
-    cidade = _empresa$endereco.cidade,
-    estado = _empresa$endereco.estado;
-console.log(nome);
-console.log(cidade);
-console.log(estado);
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return "".concat(nome, " tem ").concat(idade, " anos.");
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+//5.1.1
+var arr = [1, 2, 3, 4, 5, 6];
+var x = arr[0],
+    y = arr.slice(1);
+console.log(x); // 1
+
+console.log(y); // [2, 3, 4, 5, 6]
+//5.1.2
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-console.log(mostraInfo({
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+
+console.log(soma(1, 2)); // 3
+//5.2
+
+var usuario = {
   nome: 'Diego',
-  idade: 23
-}));
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+};
+
+var usuario2 = _objectSpread({}, usuario, {
+  nome: 'Gabriel'
+});
+
+console.log(usuario2);
+
+var usuario3 = _objectSpread({}, usuario, {
+  endereco: _objectSpread({}, usuario.endereco, {
+    cidade: 'Lontras'
+  })
+});
+
+console.log(usuario3);
