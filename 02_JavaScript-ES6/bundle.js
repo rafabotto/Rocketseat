e85a8054526c932f4cbc4c5c90905347f4cfdbfd
rafabotto <rbdf91@gmail.com>
2019-04-29 10:07:15 -0300

    exercise 02 from module 01

--- 02_JavaScript-ES6/bundle.js
+++ 02_JavaScript-ES6/bundle.js
@@ -1,63 +1,56 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+var usuarios = [{
+  nome: 'Diego',
+  idade: 23,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Gabriel',
+  idade: 15,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Lucas',
+  idade: 30,
+  empresa: 'Facebook'
+}]; //2.1
+// const idadeArr = usuarios.map(function(item){
+//     return item.idade;
+// });
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+var idadeArr = usuarios.map(function (usuario) {
+  return usuario.idade;
+});
+console.log(idadeArr); // //2.2
+// const filter = usuarios.filter(function(item){
+//     if(item.idade > 18 && item.empresa === 'Rocketseat'){
+//         return item;
+//     }
+// });
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+var filter = usuarios.filter(function (usuario) {
+  return usuario.empresa === 'Rocketseat' && usuario.idade >= 18;
+});
+console.log(filter); //2.3
+// const find = usuarios.find(function(item){
+//     if(item.empresa === 'Google'){
+//         return item;
+//     }
+// });
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+var find = usuarios.find(function (usuario) {
+  return usuario.empres === "Google";
+});
+console.log(find); //2.4
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Usuario =
-/*#__PURE__*/
-function () {
-  function Usuario(email, senha) {
-    _classCallCheck(this, Usuario);
-
-    this.email = email;
-    this.senha = senha;
-  }
-
-  _createClass(Usuario, [{
-    key: "isAdmin",
-    value: function isAdmin() {
-      return this.admin === true;
-    }
-  }]);
-
-  return Usuario;
-}();
-
-var Admin =
-/*#__PURE__*/
-function (_Usuario) {
-  _inherits(Admin, _Usuario);
-
-  function Admin(email, senha) {
-    var _this;
-
-    _classCallCheck(this, Admin);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Admin).call(this, email, senha));
-    _this.admin = true;
-    return _this;
-  }
-
-  return Admin;
-}(Usuario);
-
-var User1 = new Usuario("email@teste.com", "senha123");
-var Admin1 = new Admin("email@teste.com", "senha123");
-console.log(User1.isAdmin()); //false
-
-console.log(Admin1.isAdmin()); //true
+var filter1 = usuarios.map(function (usuario) {
+  return _objectSpread({}, usuario, {
+    idade: usuario.idade * 2
+  });
+}).filter(function (usuario) {
+  return usuario.idade <= 50;
+});
+console.log(filter1);
