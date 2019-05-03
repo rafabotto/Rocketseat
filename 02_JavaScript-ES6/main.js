cfc09b24193cdb45e56937962eefe62781827204
rafabotto <rbdf91@gmail.com>
2019-05-03 11:20:27 -0300

    configuring webpack

--- 02_JavaScript-ES6/main.js
+++ 02_JavaScript-ES6/main.js
@@ -1,18 +1,2 @@
-// const nome = 'Diego';
-// const idade = 23;
-// const usuario = {
-//  nome: nome,
-//  idade: idade,
-//  cidade: 'Rio do Sul',
-// };
-
-
-const nome = 'Diego';
-const idade = 23;
-const usuario = {
- nome,
- idade,
- cidade: 'Rio do Sul',
-};
-
-console.log(usuario);
+import { soma } from './funcoes';
+console.log(soma(1,2));
